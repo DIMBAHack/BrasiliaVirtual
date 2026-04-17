8ce1301ab8771b988b4d2a028b7c869cd97a3b0b
beatrizbranfb <beatrizbfb@icloud.com>
2026-04-17 20:00:40 -0300

    vou arrumar já já

--- api/fontes.py
+++ api/fontes.py
@@ -1,6 +1,11 @@
 from files.fileManagement import fileArrumadoAnalisar
 from database.configDB import ConfigDB
+import os
+from dotenv import load_dotenv
+
+
 
 class Fontes:
     def __init__(self):
         self.file = fileArrumadoAnalisar.chuncking()
+
